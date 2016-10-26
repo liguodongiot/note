10a2629561057e91e5b4cfbfbca93462538ccb5e
liguodong <liguodong@cd.tuan800.com>
2016-10-26 11:01:40 +0800

    移动文件版本1.1

---
+++ important/.bashrc
@@ -0,0 +1,14 @@
+
+export JAVA_HOME=/home/liguodong/install/jdk
+export SCALA_HOME=/home/liguodong/install/scala
+export PATH=$JAVA_HOME/bin:$SCALA_HOME/bin:$PATH
+
+export HADOOP_HOME=/home/liguodong/install/hadoop
+export HIVE_HOME=/home/liguodong/install/hive
+export PATH=$HADOOP_HOME/bin:$HIVE_HOME/bin:$PATH
+
+export ZOOKEEPER_HOME=/home/liguodong/install/zookeeper
+export HBASE_HOME=/home/liguodong/install/jdk
+export KYLIN_HOME=/home/liguodong/install/scala
+export PATH=$ZOOKEEPER_HOME/bin:$HBASE_HOME/bin:$KYLIN_HOME/bin:$PATH
+
--- shell/move_file.sh
+++ shell/move_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # ----------------------------------------------------------------------
-# name:         move_file.sh
-# version:      1.0
-# createTime:   2016-10-19
+# name:         move_file_1.1.sh
+# version:      1.1
+# createTime:   2016-10-26
 # description:  移动压缩文件、解压缩、重命名
 # author:       liguodong
 # email:        liguodongiot@gmail.com
@@ -15,6 +15,7 @@ echo "开始执行脚本..."
 if [ ! $# == 2 ]; then
 
 echo "Usage: $0 targetFileName renameFileName"
+echo "如果无需重命名，请将参数2传递为空字符串... "
 exit
 
 fi 
@@ -45,16 +46,35 @@ echo "当前路径为:"
 path=`pwd`
 echo "${path}"
 
-echo "正在解压缩指定文件..."
-tar -zxvf ${1}
+file=$1
+
+if [ "${file##*.}"x = "gz"x ]||[ "${file##*.}"x = "tgz"x ]
+then
+	echo "正在解压缩指定文件${file}..."
+	tar -zxvf ${file}
+elif [ "${file##*.}"x = "zip"x ]
+then
+	echo "正在解压缩指定文件${file}..."
+	unzip ${file}
+else
+	echo "${file}不符合格式要求，即将退出。。。"
+	exit 1
+fi
+
 
 echo "正在删除压缩文件..."
 rm ${1}
 
-echo "正在重命名解压缩文件..."
-temp="*"${2}"*"
 
-mv ${temp} ${2}
+if [ "${2}" = "" ]
+then
+	echo "参数2为空字符串，无需重命名。。。"
+else
+	echo "正在重命名解压缩文件..."
+	temp="*"${2}"*"
+	mv ${temp} ${2}
+fi
+
 
 echo "执行脚本结束..."
 
